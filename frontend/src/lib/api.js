6a6dc136dc29cc411bc691b0d8d209e8a2ca55c3
shreeyareddy-07 <shreeyareddy07@gmail.com>
2026-07-11 05:56:22 +0000

    Add Auth Header

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -7,6 +7,13 @@ export const api = axios.create({
   baseURL: API,
   withCredentials: true,
 });
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export function formatApiError(err) {
   const detail = err?.response?.data?.detail;
