7754d8fc26208dfa980a0aade9b27416246ef605
shreeyareddy-07 <shreeyareddy07@gmail.com>
2026-07-11 15:19:52 +0000

    Fix request authorization header

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -11,6 +11,7 @@ export const api = axios.create({
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
+    config.headers = config.headers || {};
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
